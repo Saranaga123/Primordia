867231503f92c0452babbc05d94bec313b58a1ca
saranga123 <asithasd1995@gmail.com>
2025-09-02 13:43:20 +0530

    vast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
- 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component'; 
-import { WormComponent } from './components/Actors/Player/common/worm/worm.component';
-import { CocoonComponent } from './components/Actors/Player/common/cocoon/cocoon.component';
-import { RadiantButterflyComponent } from './components/Actors/Player/cosmic/radiant-butterfly/radiant-butterfly.component';
-import { TargetComponent } from './components/minigame/target/target.component';
-import { LoginComponent } from './pages/login/login.component';
-import { StoryComponent } from './pages/story/story.component';
-import { DefeatedComponent } from './pages/defeated/defeated.component';
-import { FightComponent } from './pages/fight/fight.component';
-import { MapComponent } from './pages/map/map.component';
-import { StatsComponent } from './pages/stats/stats.component';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
-    WormComponent, CocoonComponent, RadiantButterflyComponent, TargetComponent, LoginComponent, StoryComponent, DefeatedComponent, FightComponent, MapComponent, StatsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
